6fb0488244634a938e251f631a7883c6f7306ad1
mohanadft <mohanadfteha@gmail.com>
2023-05-08 15:58:14 +0300

    Add feature to store current state of theme in local storage
    This commit adds a new feature to the application that stores the current state of the theme in local storage. This means that when the user reloads the page, the theme will not reset to the default.

--- js/main.js
+++ js/main.js
@@ -1,21 +1,40 @@
-const switchTheme = function () {
+const setTheme = isDark => {
 	const root = document.querySelector(':root')
-	let isDark = false
 
-	return () => {
-		isDark = !isDark
+	const theme = {
+		'--bg_default': isDark ? '#1a1a1a' : '#fff',
+		'--bg_body': isDark ? '#282828' : '#f0f9ff',
+		'--lines-color': isDark ? '#000' : '#ddd',
+		'--body-text': isDark ? '#ededed' : '#333'
+	}
 
-		const theme = {
-			'--bg_default': isDark ? '#1a1a1a' : '#fff',
-			'--bg_body': isDark ? '#282828' : '#f0f9ff',
-			'--lines-color': isDark ? '#000' : '#ddd',
-			'--body-text': isDark ? '#ededed' : '#333'
-		}
-		for (let v in theme) {
-			root.style.setProperty(v, theme[v])
-		}
+	for (let v in theme) {
+		root.style.setProperty(v, theme[v])
 	}
 }
+
+const checkLocalStorage = () => {
+	let isDark = localStorage.getItem('isDark')
+
+	if (isDark !== null) {
+		isDark = JSON.parse(isDark)
+		setTheme(isDark)
+	} else setTheme(false)
+}
+
+const switchTheme = function () {
+	return () => {
+		localStorage.setItem(
+			'isDark',
+			JSON.stringify(!JSON.parse(localStorage.getItem('isDark')))
+		)
+
+		let isDark = JSON.parse(localStorage.getItem('isDark'))
+		setTheme(isDark)
+	}
+}
+
+checkLocalStorage()
 
 let themeBtn = document.getElementById('theme-btn')
 let _switchTheme = switchTheme()
